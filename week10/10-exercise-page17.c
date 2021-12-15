8f90adeb1b705d490145ddf67fe2cbab45cd7f79
Pham Tien Loc 20205095 <87697310+MatsuoRotsuki@users.noreply.github.com>
2021-12-15 22:38:53 +0700

    Add files via upload

---
+++ week10/10-exercise-page17.c
@@ -0,0 +1,8 @@
+// Exercise: Comparison of 
+// running time
+// • Write a program to initiate an array of 500 
+// integers by using random function.
+// • Sort this array using insertion sort and 
+// heap sort. Calculate the running time of 
+// program in each case and print out the 
+// results.
